c44532a62f8d82702d7d1af4ec0ca1e7d9a0d093
dramaticlly <steveiszhy@gmail.com>
2015-05-21 21:43:44 -0400

    construct and push and print node

--- reverse.c
+++ reverse.c
@@ -1,8 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* int x;
+ int *p;  // p is integer pointer
+
+ x = 0;   // now x == 0
+ p = &x;  // & takes the address of x
+          // now p == &x, so *p == x
+ *p = 1;  // equivalent to x = 1, since *p == x
+ */
+
+
+
+
+typedef struct nodeStruct{
+	int data;
+	struct nodeStruct *next;
+} node;
+
+void push(int _data, node **head) 
+{
+	/* data */
+	node *temp = malloc(sizeof(node));
+	temp->data = _data;
+	temp->next = *head;
+	*head = temp;
+}
+
+void printNode(node* _head)
+{
+	while(_head != NULL)
+	{
+		printf("%d ", _head->data);
+		_head = _head->next;
+	}
+	printf("\n");
+}
+
 int main()
 {
 	printf("Hello World. \n");
+	node *head = NULL;
+	push(&head,2);
+	push(&head,4);
+	push(&head,6);
+	push(&head,7);
+	push(&head,8);
+	printNode(head);
+
 	return 0;
 }
